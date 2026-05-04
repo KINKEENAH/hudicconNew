6858c278e120aa273de030d05426803f584690d6
KINKEENAH <sakinabintmustapha@gmail.com>
2026-05-04 08:22:57 +0000

    much more fixing

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,3 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../config/db");
+
 // POST /api/contact
 router.post("/", async (req, res) => {
   const { name, email, subject, message } = req.body;
